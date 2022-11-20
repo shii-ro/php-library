1469ca93a49688512a18ce2a5b5e70972710865f
shii-ro <wesley.7.castro@gmail.com>
2022-11-19 23:08:16 -0300

    Initial commit

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,5 +1,5 @@
 <?php  //connect using mySQLi
-    $connection = mysqli_connect('localhost', 'adolf', 'hitler', 'library');
+    $connection = mysqli_connect('', '', '', 'library');
     if (!$connection) { // connection is sucessful?
         echo 'Connection error ' . mysqli_connect_error();
     }
